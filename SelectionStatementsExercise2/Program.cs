16114e0e1ca737a799be8b0739491361c655ed57
Kaddijatou Hydara <kad67414@gmail.com>
2025-01-20 10:20:21 -0500

    add work 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace SelectionStatementsExercise2
+﻿using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SelectionStatementsExercise2
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            FavSubject();
+        }
+
+        public static void FavSubject() 
+        {   
+            Console.WriteLine("Welcome to Fav Subject");
+        
+            String subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("I don't prefer math ");
+                    break;
+                case "science":
+                    Console.WriteLine("Science rules");
+                    break;
+                case "pe":
+                case "p.e":
+                case "physical education":
+                    Console.WriteLine("P.E. is fun");
+                    break;
+                case "history":
+                    Console.WriteLine("History rules");
+                    break;
+                case "geography":
+                    Console.WriteLine("I like to study the world");
+                    break;
+                default:
+                    Console.WriteLine("I like that too");
+                    break;
+                
+            }
+            
         }
     }
-}
